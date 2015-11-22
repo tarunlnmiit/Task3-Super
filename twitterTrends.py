94c8357478b2abb106c7470e0130a56df167671d
tarunlnmiit <tarungupta.y12@lnmiit.ac.in>
2015-11-22 11:04:14 +0530

    Seventh commit

--- twitterTrends.py
+++ twitterTrends.py
@@ -1,7 +1,4 @@
-# https://api.twitter.com/1.1/trends/place.json/?id=1&oauth_consumer_key=mdkejxebqytj99rnkyhc1mcte&oauth_nonce=0708a4359f7a249405ccda85750a5d65&oauth_signature_method=hmac-sha1&oauth_timestamp=1448114158&oauth_token=1100686363-6jxqino5t8umpqewyah783qrxxjjrmwbljcetof&oauth_version=1.0
-
 import twitter
-import json
 
 # auth = twitter.oauth.OAuth('1100686363-6JxQIno5T8UMPQeWyaH783qrxxJJrmwbLJCEtoF', 'GqgnME3MRfOQL79TzWuH5S9fJOgXXogdAwyVDzGSk4ovD', 'mDkejxEBQYTj99RNkYhc1mCTE', 'WR2xlMAu4gRelaPRDkywDXCAGldZOZT6u4AymUBE0MpyjtooCg')
 
@@ -11,7 +8,7 @@ twitter_api = twitter.Twitter(auth=auth)
 
 world_woeid = 1
 mumbai_woeid = 2295411
-#delhi_woeid = 28743736
+#delhi_woeid = 2295019
 pune_woeid = 2295412
 jaipur_woeid = 2295401
 kolkata_woeid = 2295386
@@ -27,7 +24,6 @@ kolkata_trends = twitter_api.trends.place(_id=kolkata_woeid)
 chennai_trends = twitter_api.trends.place(_id=chennai_woeid)
 bangalore_trends = twitter_api.trends.place(_id=bangalore_woeid)
 
-#print json.dumps(wt, indent=1)
 
 world_trends_set = set([trend['name'] for trend in world_trends[0]['trends']])
 mumbai_trends_set = set([trend['name'] for trend in mumbai_trends[0]['trends']])
@@ -48,37 +44,10 @@ common_world_bangalore = world_trends_set.intersection(bangalore_trends_set)
 
 trends = {'World': world_trends_set, 'Mumbai': mumbai_trends_set, 'Pune': pune_trends_set, 'Jaipur': jaipur_trends_set , 'Kolkata': kolkata_trends_set, 'Chennai': chennai_trends_set, 'Bangalore': bangalore_trends_set}
 
-print '\n World_trends:'
-for item in world_trends_set:
-	if '#' in item:	print item,
-
-print '\n Mumbai_trends:',
-for item in mumbai_trends_set:
-	if '#' in item:	print item,
-
-# print '\n Delhi_trends:'
-# for item in  delhi_trends_set:
-# 	if '#' in item:	print item,
-
-print '\n Pune_trends:',
-for item in  pune_trends_set:
-	if '#' in item:	print item,
-
-print '\n Jaipur_trends:',
-for item in  jaipur_trends_set:
-	if '#' in item:	print item,
-
-print '\n Kolkata_trends:',
-for item in  kolkata_trends_set:
-	if '#' in item:	print item,
-
-print '\n Chennai_trends:',
-for item in  chennai_trends_set:
-	if '#' in item:	print item,
-
-print '\n Bangalore_trends:',
-for item in  bangalore_trends_set:
-	if '#' in item:	print item,
+for key, value in trends.iteritems():
+	print '\n', key+':',
+	for item in value:
+		if '#' in item:	print item,
 
 print
 print
@@ -94,5 +63,5 @@ for i in xrange(len(key)):
 				if '#'in item:
 					print item, 
 		else:
-			print 'None'
+			print 'None',
 		print
